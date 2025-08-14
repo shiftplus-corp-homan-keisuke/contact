037f63bef2f344260379e7abaab4f96ddff830f7
homan <homan@shiftplus.co.jp>
2025-08-14 10:42:21 +0900

    reset

--- src/common/decorators/public.decorator.ts
+++
@@ -1,9 +0,0 @@
-/**
- * パブリックエンドポイント用デコレーター
- * 認証を不要にするためのデコレーター
- */
-
-import { SetMetadata } from '@nestjs/common';
-
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
